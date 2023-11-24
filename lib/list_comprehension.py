214213bf2891e2199b59ef1ae33a7e2d81fa4d8b
Eckra Wangui <+>
2023-11-24 13:36:43 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    for n in range(0,11):
+        return[element for element in num_list if element % 2==0]
     pass
 
 def make_exclamation(sentence_list):
+    return[sentence + "!" for sentence in sentence_list]
     pass
